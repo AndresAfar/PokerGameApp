0c4863321c3e0f65ab1c54088ca38968468130d7
AndresAfar <afar2503@gmail.com>
2024-04-12 09:07:57 -0500

    add page

---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const MainPage = () => {
+  return (
+    <>
+
+    <div className='content-game mt-14 w-full h-[600px] bg-white mx-auto'>
+
+    </div>
+    </>
+  )
+}
