ce02e499049cfba254931ed3667048f2426b32c9
AndresAfar <afar2503@gmail.com>
2024-04-12 09:05:02 -0500

    modified formlogin styles

--- client/src/components/forms/FormLogin.jsx
+++ client/src/components/forms/FormLogin.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
 
 export const FormLogin = () => {
   return (
 
     <>
-    <div className='max-w-sm mx-auto bg-slate-800 rounded-lg border-4 border-slate-700 p-10'>
+    <a href="/" class="text-sm font-medium text-primary-600 hover:underline text-gray-300">Regresar</a>
+    <div className='max-w-sm mx-auto bg-slate-800 rounded-lg border-4 border-slate-700 p-6 md:p-10'>
       <div className='py-4'>
         <label className='text-4xl'>Iniciar Sesión</label>
       </div>
@@ -17,7 +19,7 @@ export const FormLogin = () => {
                   <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
                 </svg>
               </span>
-              <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-500 text-gray-900 focus:ring-emerald-500 focus:border-emerald-500 block flex-1 min-w-0 w-full text-sm p-2.5" placeholder="name@dominio.com" required/>
+              <input type="text" id="username" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-500 text-gray-900 focus:ring-emerald-500 focus:border-emerald-500 block flex-1 min-w-0 w-full text-sm p-2.5" placeholder="name@dominio.com" required/>
             </div>
           </div>
           <div class="mb-5">
@@ -28,10 +30,18 @@ export const FormLogin = () => {
                   <path fill-rule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z" clip-rule="evenodd"/>
                 </svg>
               </span>
-              <input type="password" id="password" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-500 text-gray-900 focus:ring-emerald-500 focus:border-emerald-500 block flex-1 min-w-0 w-full text-sm p-2.5" required/>
+              <input type="password" id="password" name="password" placeholder="••••••••" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-500 text-gray-900 focus:ring-emerald-500 focus:border-emerald-500 block flex-1 min-w-0 w-full text-sm p-2.5" required/>
             </div>
+            <a href="#" class="text-sm font-medium text-primary-600 hover:underline text-gray-300">¿Ha olvidado su contraseña?</a>
           </div>
-          <button type="submit" class="text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Iniciar Sesión</button>
+          <button type="submit"  class="w-full my-2 text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+            <Link to='/ini'>
+              Iniciar Sesion
+            </Link>
+          </button>
+          <p class="text-sm font-light text-gray-400">
+            ¿Aún no tienes cuenta? <a href="#" class="font-medium text-gray-300 hover:underline ">Registrarse</a>
+          </p>
       </form>
     </div>
     </>
